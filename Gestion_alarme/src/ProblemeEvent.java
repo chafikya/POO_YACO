83a7b4a357bb9bc20d8c6817457b277075defa00
coltarm <113551889+coltarm@users.noreply.github.com>
2023-05-02 09:31:24 +0200

    pbevent

--- Gestion_alarme/src/ProblemeEvent.java
+++ Gestion_alarme/src/ProblemeEvent.java
@@ -1,13 +1,14 @@
-import java.util.EventObject;
-
 public class ProblemeEvent extends EventObject{
 	String type_capteur;
 	float valeur;
+	String nom;
+	String lieu;
 	
 	
-	public ProblemeEvent(Object source, String un_type, float une_valeur) {
+	public ProblemeEvent(Object source, String un_type, String un_lieu, float une_valeur, String un_nom) {
 		super(source);
 		this.valeur = une_valeur;
 		this.type_capteur = un_type;
+		this.nom = un_nom;
+		this.lieu= un_lieu;
 	}
-}
