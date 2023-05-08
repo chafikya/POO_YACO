cc541d32254e5c5f001fbf1c23775904f712d063
coltarm <113551889+coltarm@users.noreply.github.com>
2023-05-08 18:34:02 +0200

    transmission entre génération d'alarme et interface

--- Gestion_alarme_ya/src/CapteurListener.java
+++
@@ -1,4 +0,0 @@
-
-public interface CapteurListener {
-	public void PbSeuille(ProblemeEvent probleme);
-	}
--- Gestion_alarme_ya/src/MoniteurB.java
+++
@@ -1,9 +0,0 @@
-
-public class MoniteurB implements CapteurListener{
-	String nom ="Service environement";
-	public MoniteurB() {}
-	public void PbSeuille(ProblemeEvent probleme) {
-		System.out.println("pb");
-	}
-
-}
